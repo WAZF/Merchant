b0ccc9611129203311e7773d96a83e065dac5447
Mohamed Washif <106472678+WAZF@users.noreply.github.com>
2024-07-02 18:30:31 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mayilai-Mart<br>
+search offer for offline products<br>
+here we make a demonstartive webapp for know the offers<br>
+username:wasif<br>
+password:wasif121<br>
+use this to enter our page(or)skip and directly use without account-login! <br>
+![coders](https://user-images.githubusercontent.com/106472678/200168526-f2ca8d23-5562-4c3a-a66d-8bf9087a13e0.png)
+![image](https://user-images.githubusercontent.com/106472678/200168872-898ab0de-b153-498a-a98b-ad42f3903b86.png)
+![image](https://user-images.githubusercontent.com/106472678/200168657-f61676c1-0b1f-477d-9182-f53e49589a8a.png)
+![image](https://user-images.githubusercontent.com/106472678/200168855-064ed973-f0da-4188-a773-e175db1bb6a4.png)
+![image](https://user-images.githubusercontent.com/106472678/200168753-1f7f9cbe-ac35-4b6b-b051-4e980f55efa9.png)
+![image](https://user-images.githubusercontent.com/106472678/200169034-261b6f7f-58e0-436a-930d-2d93b9148ed0.png)
+![image](https://user-images.githubusercontent.com/106472678/200169330-763023ca-4bfc-46c7-954f-4c46e6bfcd65.png)
+
+
---
+++ script.js
@@ -0,0 +1,54 @@
+var token = ""
+document.getElementById('buyNow').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default action of the link
+
+    // Get the form data
+    const purchaseId = "0e22649f-3217-4374-816b-99011f459cc1";
+    const userId = "1234567";
+    const emailid = "someone@gmail.com";
+    const mobileNumber = "7878787878";
+
+    // Define the data to be sent in the request body
+    const data = {
+        purchaseId: purchaseId,
+        ucic: userId,
+        emailId: emailid,
+        mobileNo: mobileNumber
+    };
+    console.log('Data',data);
+
+    // Make the fetch call with a JSON body
+    fetch('https://marketplace.digiledge.in/marketplace-simulationserver/marketplace/MPINPayment', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // Convert the data to a JSON string
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+        // Navigate to the target page after the API call is successful
+        // window.location.href = 'index.html';
+        token = responseData.token;
+        window.alert(`Success: ${responseData.statusMessage}\nToken: ${responseData.token}`);
+    })
+    .catch((error) => {
+        console.log('Error:', error);
+        // Optionally, handle the error (e.g., display an error message)
+        window.alert(`Error: ${error.message}`);
+    });
+});
+
+function invokeMPINFromWebPage() {
+    console.log("inside invokeMPIN function")
+    var orderDetails = {
+        accountNumber: "1234567890", 
+        purchaseToken: token,   
+        amount: 100,                 
+        timestamp: new Date().toISOString() 
+    };
+    var messageBody = JSON.stringify(orderDetails);
+    console.log('messageBody',messageBody)
+    window.platformInterface.invokeMPIN(messageBody);
+}

d33497d8e62e20ea5b42aef6fefc35f1e30a34f4
Mohamed Washif <106472678+WAZF@users.noreply.github.com>
2025-01-06 12:39:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ document.getElementById('buyNow').addEventListener('click', function(event) {
         mode: 'cors',
         method: 'POST',
         headers: {
+            'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Accept',
             'Content-Type': 'application/json'
         },
